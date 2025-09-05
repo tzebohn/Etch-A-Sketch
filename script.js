31a1f5ee5f8f7ee4533320b599ed010552235251
tzebohn <tzebohnling@gmail.com>
2025-09-04 21:34:31 -0400

    Created the canvas, where user can draw
    Added functions to generate and paint the squares.
    Added event listeners to allow the user to draw via dragging
    Added CSS styling to make each square takes equal space.

--- script.js
+++ script.js
@@ -25,4 +25,47 @@ sliderValue.textContent = `${sliderInput.value}x${sliderInput.value}`
 //Add event listener for sliderInput
 sliderInput.addEventListener("input", () => {
     sliderValue.textContent = `${sliderInput.value}x${sliderInput.value}` //Update display
+    generateCanvas(sliderInput.value) //Generate the 
 })
+
+//Generate canvas with slider input
+const canvas = document.getElementById("canvas")
+
+//Allow a click and drag to draw feature for user
+let isMouseDown = false
+document.addEventListener("mousedown", () => isMouseDown = true)
+document.addEventListener("mouseup", () => isMouseDown = false)
+
+//Generate the default canvas
+generateCanvas(sliderInput.value)
+
+// This function takes in the slider input and generates canvas
+function generateCanvas(num) {
+    //Clear previous canvas
+    canvas.innerHTML = ""
+
+    //Calculate the width of each square
+    let width = 100 / num
+
+    //Create the squares
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement("div")
+        square.className = "square"
+        square.style.width = `${width}%`
+        square.addEventListener("mousedown", paint)
+        square.addEventListener("mouseenter", (e) => { 
+            if(isMouseDown) {
+                console.log("drag drawing")
+                paint(e)
+            }
+        })
+        canvas.appendChild(square)
+    }
+}
+
+//Paints the corresponding square
+function paint(e) {
+    console.log("called")
+    e.target.style.backgroundColor = colorInput.value
+}
+
